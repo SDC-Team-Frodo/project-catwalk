4bc8469db52acd77d83c95b7f43c9ef3a748331b
Alec <alecech1994@gmail.com>
2021-06-04 11:56:52 -0700

    Added get route to get information from Atelier API

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const config = require('../config/config');
+require('dotenv').config();
+
+// process.env.ENV_VAR_NAME
 
 const app = express();
 const PORT = 42069;
@@ -8,6 +13,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+// routes
+app.get('/api', (req, res) => {
+  const { endpoint, data } = req.body;
+  const options = {
+    url: `${config.API_HOST}/${endpoint}`,
+    method: 'GET',
+    headers: {
+      'User-Agent': 'request',
+      Authorization: `${process.env.GIT_KEY}`,
+    },
+    data,
+  };
+
+  axios(options)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
