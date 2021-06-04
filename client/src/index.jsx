54c6db9af069c16ae5a4cedb590334b38af890a6
Alec <alecech1994@gmail.com>
2021-06-04 11:57:33 -0700

    Updated environment variables and config authorization

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+axios.get('/api', {
+  endpoint: 'products/69696',
+  data: {
+    count: 25,
+    page: 2,
+  },
+});
---
+++ config/config.js
@@ -0,0 +1,6 @@
+const CAMPUS_CODE = 'hr-rfp';
+const API_HOST = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS_CODE}`;
+
+module.exports = {
+  API_HOST,
+};
