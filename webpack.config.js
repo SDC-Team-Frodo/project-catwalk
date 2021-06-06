14dede9b742ca0bed3d0248237cc092012b9037a
Alec <alecech1994@gmail.com>
2021-06-06 01:05:47 -0700

    Minor updates to optimize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,7 @@ module.exports = {
       {
         test: /\.jsx?/,
         loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
+        exclude: /node_modules\/.*/,
       },
       {
         test: /\.s[ac]ss$/i,
