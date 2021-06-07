ce56fede7b83786c3494535e0d886089093b711c
Alec <alecech1994@gmail.com>
2021-06-07 09:33:06 -0700

    Checking circleci with failing test

--- client/src/components/ReviewsWidget/widget.test.js
+++ client/src/components/ReviewsWidget/widget.test.js
@@ -9,7 +9,7 @@ describe('Review component', () => {
   });
 
   it('should have the right message in the dom', () => {
-    const message = 'this is the Review';
+    const message = 'this is Review';
 
     expect(screen.getByText(message)).toBeInTheDocument();
   });
