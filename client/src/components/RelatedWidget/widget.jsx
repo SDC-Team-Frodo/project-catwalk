f370b26130972b09275a585cd4839b6aab992217
Francis Yao <neko.makie27@gmail.com>
2021-06-07 09:36:15 -0700

    Pull test

--- client/src/components/RelatedWidget/widget.jsx
+++ client/src/components/RelatedWidget/widget.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const RelatedContainer = (props) => (
-  <div id="Related">
-    <h1>this is the Related</h1>
-  </div>
+
+
+  return (
+    <div id="Related">
+      <h1>this is the Related</h1>
+    </div>
+  );
 );
 
 export default RelatedContainer;
