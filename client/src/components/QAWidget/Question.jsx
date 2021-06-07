f627fb4a05e748f10aed475b59e879c64dffeab8
Jacob Santala <JacobISantala@gmail.com>
2021-06-07 16:47:11 -0700

    Will now load only 4 questions and 2 answers for each question, and you are able to render more by clicking the buttons.

--- client/src/components/QAWidget/Question.jsx
+++ client/src/components/QAWidget/Question.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Answer from './Answer';
+
 
 const Question = (props) => {
   const { question } = props;
+  const allAnswers = Object.values(question.answers);
+  const [displayedAnswers, setDisplayedAnswers] = useState(2);
+  const [answers, setAnswers] = useState(allAnswers.slice(0, displayedAnswers));
+  const [buttonLabel, setButtonLabel] = useState('More Answers');
+
+  useEffect(() => {
+    if (allAnswers[displayedAnswers - 2] !== undefined) {
+      setAnswers(allAnswers.slice(0, displayedAnswers));
+    } else {
+      setButtonLabel('No More Answers');
+    }
+  }, [displayedAnswers]);
   return (
     <div>
-      <h1>{`Q: ${question["question_body"]}`}</h1>
-      {Object.keys(question.answers).map((key, i) => {
-        let current = question.answers[key];
+      <h1>{`Q: ${question.question_body}`}</h1>
+      {answers.map((answer, i) => {
         return (
-          <Answer answer={current} key={i} />
+          <Answer answer={answer} key={i} />
         );
       })}
+      <button id="loadA" onClick={
+        () => {
+          setDisplayedAnswers(displayedAnswers + 2)
+        }
+      }>{buttonLabel}</button>
     </div>
   );
 };
--- client/src/components/QAWidget/mockData.js
+++ client/src/components/QAWidget/mockData.js
@@ -1,93 +1,256 @@
 const mockData = {
-  "product_id": "17072",
+  "product_id": "17073",
   "results": [
       {
-          "question_id": 153103,
-          "question_body": "What's a good way to spend my Sunday?",
-          "question_date": "2021-03-10T00:00:00.000Z",
-          "asker_name": "slenderMan",
-          "question_helpfulness": 16,
+          "question_id": 104636,
+          "question_body": "What fabric is the top made of?",
+          "question_date": "2019-08-18T00:00:00.000Z",
+          "asker_name": "coolkid",
+          "question_helpfulness": 9,
           "reported": false,
           "answers": {
-              "1444036": {
-                  "id": 1444036,
-                  "body": "Go to Chick-fil...Nvm",
-                  "date": "2021-03-11T00:00:00.000Z",
-                  "answerer_name": "theColonel",
-                  "helpfulness": 4,
+              "992173": {
+                  "id": 992173,
+                  "body": "Suede",
+                  "date": "2019-09-18T00:00:00.000Z",
+                  "answerer_name": "warmkid",
+                  "helpfulness": 0,
+                  "photos": [
+                      "https://images.unsplash.com/photo-1548430395-ec39eaf2aa1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1974&q=80"
+                  ]
+              },
+              "992174": {
+                  "id": 992174,
+                  "body": "Something pretty soft but I can't be sure",
+                  "date": "2019-09-18T00:00:00.000Z",
+                  "answerer_name": "warmkid",
+                  "helpfulness": 1,
                   "photos": []
               },
-              "1444045": {
-                  "id": 1444045,
-                  "body": "Check, ch-check check check ch-check it out!",
-                  "date": "2021-03-11T00:00:00.000Z",
-                  "answerer_name": "Beastie Boys",
+              "992185": {
+                  "id": 992185,
+                  "body": "Its the best! Seriously magic fabric",
+                  "date": "2019-09-18T00:00:00.000Z",
+                  "answerer_name": "warmkid",
+                  "helpfulness": 1,
+                  "photos": []
+              },
+              "992186": {
+                  "id": 992186,
+                  "body": "Supposedly suede, but I think its synthetic",
+                  "date": "2019-09-18T00:00:00.000Z",
+                  "answerer_name": "warmkid",
                   "helpfulness": 2,
-                  "photos": []
-              },
-              "1444095": {
-                  "id": 1444095,
-                  "body": "I think that is a splendid question",
-                  "date": "2021-03-12T00:00:00.000Z",
-                  "answerer_name": "askjeeves",
-                  "helpfulness": 0,
-                  "photos": []
-              },
-              "1444212": {
-                  "id": 1444212,
-                  "body": "Everything is ok!",
-                  "date": "2021-03-12T00:00:00.000Z",
-                  "answerer_name": "theKidsAreAlright",
-                  "helpfulness": 0,
                   "photos": []
               }
           }
       },
       {
-          "question_id": 153372,
-          "question_body": "Great Q, here is an answer",
-          "question_date": "2021-03-12T00:00:00.000Z",
-          "asker_name": "",
-          "question_helpfulness": 2,
+          "question_id": 104638,
+          "question_body": "Why is this product cheaper here than other sites?",
+          "question_date": "2019-02-04T00:00:00.000Z",
+          "asker_name": "l33tgamer",
+          "question_helpfulness": 5,
           "reported": false,
-          "answers": {
-              "1444114": {
-                  "id": 1444114,
-                  "body": "Why yessss of course",
-                  "date": "2021-03-12T00:00:00.000Z",
-                  "answerer_name": "anon",
-                  "helpfulness": 1,
-                  "photos": []
-              }
-          }
+          "answers": {}
       },
       {
-          "question_id": 153261,
-          "question_body": "I want to know, can you show me?",
-          "question_date": "2021-03-11T00:00:00.000Z",
-          "asker_name": "PhilCollins",
-          "question_helpfulness": 2,
-          "reported": false,
-          "answers": {
-              "1444001": {
-                  "id": 1444001,
-                  "body": "I WANNA KNOW ABOUT THESE STRANGERS LIKE ME!",
-                  "date": "2021-03-11T00:00:00.000Z",
-                  "answerer_name": "Tarzan",
-                  "helpfulness": 2,
-                  "photos": []
-              },
-              "1444039": {
-                  "id": 1444039,
-                  "body": "You can't use my songs!",
-                  "date": "2021-03-11T00:00:00.000Z",
-                  "answerer_name": "PhilCollins",
-                  "helpfulness": 1,
-                  "photos": []
-              }
+        "question_id": 104639,
+        "question_body": "What fabric is the top made of?",
+        "question_date": "2018-02-12T00:00:00.000Z",
+        "asker_name": "mrmrs",
+        "question_helpfulness": 7,
+        "reported": false,
+        "answers": {
+            "992153": {
+                "id": 992153,
+                "body": "Its the best! Seriously magic fabric",
+                "date": "2018-03-12T00:00:00.000Z",
+                "answerer_name": "mrmrs",
+                "helpfulness": 3,
+                "photos": []
+            },
+            "992158": {
+                "id": 992158,
+                "body": "Supposedly suede, but I think its synthetic",
+                "date": "2018-03-12T00:00:00.000Z",
+                "answerer_name": "mrmrs",
+                "helpfulness": 3,
+                "photos": []
+            },
+            "992182": {
+                "id": 992182,
+                "body": "Suede",
+                "date": "2018-03-12T00:00:00.000Z",
+                "answerer_name": "mrmrs",
+                "helpfulness": 9,
+                "photos": []
+            },
+            "1443675": {
+                "id": 1443675,
+                "body": "Here are pictures!",
+                "date": "2021-03-08T00:00:00.000Z",
+                "answerer_name": "ParrotFan",
+                "helpfulness": 0,
+                "photos": [
+                    "https://unsplash.com/photos/n0uRXGaHesY"
+                ]
+            }
+        }
+    },
+    {
+        "question_id": 104641,
+        "question_body": "Can I wash it?",
+        "question_date": "2018-09-24T00:00:00.000Z",
+        "asker_name": "cleopatra",
+        "question_helpfulness": 5,
+        "reported": false,
+        "answers": {
+            "992143": {
+                "id": 992143,
+                "body": "I've thrown it in the wash and it seems fine",
+                "date": "2018-10-24T00:00:00.000Z",
+                "answerer_name": "marcanthony",
+                "helpfulness": 1,
+                "photos": []
+            },
+            "992144": {
+                "id": 992144,
+                "body": "It says not to",
+                "date": "2018-10-24T00:00:00.000Z",
+                "answerer_name": "marcanthony",
+                "helpfulness": 12,
+                "photos": []
+            },
+            "992145": {
+                "id": 992145,
+                "body": "Yes",
+                "date": "2018-10-24T00:00:00.000Z",
+                "answerer_name": "ceasar",
+                "helpfulness": 3,
+                "photos": []
+            },
+            "992175": {
+                "id": 992175,
+                "body": "I wouldn't machine wash it",
+                "date": "2018-10-24T00:00:00.000Z",
+                "answerer_name": "ceasar",
+                "helpfulness": 2,
+                "photos": []
+            },
+            "992202": {
+                "id": 992202,
+                "body": "Only if you want to ruin it!",
+                "date": "2018-10-24T00:00:00.000Z",
+                "answerer_name": "ceasar",
+                "helpfulness": 1,
+                "photos": []
+            }
+        }
+    },
+    {
+      "question_id": 104645,
+      "question_body": "What fabric is the bottom made of?",
+      "question_date": "2019-06-28T00:00:00.000Z",
+      "asker_name": "l33tgamer",
+      "question_helpfulness": 1,
+      "reported": false,
+      "answers": {
+          "992150": {
+              "id": 992150,
+              "body": "Its a rubber sole",
+              "date": "2019-11-28T00:00:00.000Z",
+              "answerer_name": "n00bgamer",
+              "helpfulness": 8,
+              "photos": []
+          },
+          "992151": {
+              "id": 992151,
+              "body": "Some kind of recycled rubber, works great!",
+              "date": "2019-11-28T00:00:00.000Z",
+              "answerer_name": "n00bgamer",
+              "helpfulness": 9,
+              "photos": []
+          },
+          "992152": {
+              "id": 992152,
+              "body": "The rubber on the bottom wears thin quickly",
+              "date": "2019-11-28T00:00:00.000Z",
+              "answerer_name": "n00bgamer",
+              "helpfulness": 7,
+              "photos": []
+          },
+          "992168": {
+              "id": 992168,
+              "body": "Rubber",
+              "date": "2019-11-28T00:00:00.000Z",
+              "answerer_name": "n00bgamer",
+              "helpfulness": 7,
+              "photos": []
           }
       }
+  },
+  {
+    "question_id": 104648,
+    "question_body": "Where is this product made?",
+    "question_date": "2018-02-28T00:00:00.000Z",
+    "asker_name": "funnygirl",
+    "question_helpfulness": 16,
+    "reported": false,
+    "answers": {
+        "992189": {
+            "id": 992189,
+            "body": "China",
+            "date": "2018-03-28T00:00:00.000Z",
+            "answerer_name": "sillyguy",
+            "helpfulness": 11,
+            "photos": []
+        }
+    }
+},
+{
+    "question_id": 104649,
+    "question_body": "What fabric is the top made of?",
+    "question_date": "2019-09-12T00:00:00.000Z",
+    "asker_name": "l33tgamer",
+    "question_helpfulness": 6,
+    "reported": false,
+    "answers": {
+        "992171": {
+            "id": 992171,
+            "body": "Supposedly suede, but I think its synthetic",
+            "date": "2019-10-12T00:00:00.000Z",
+            "answerer_name": "n00bgamer",
+            "helpfulness": 4,
+            "photos": []
+        },
+        "992190": {
+            "id": 992190,
+            "body": "Suede",
+            "date": "2019-10-12T00:00:00.000Z",
+            "answerer_name": "n00bgamer",
+            "helpfulness": 2,
+            "photos": []
+        },
+        "992191": {
+            "id": 992191,
+            "body": "Something pretty soft but I can't be sure",
+            "date": "2019-10-12T00:00:00.000Z",
+            "answerer_name": "n00bgamer",
+            "helpfulness": 3,
+            "photos": []
+        },
+        "992192": {
+            "id": 992192,
+            "body": "Its the best! Seriously magic fabric",
+            "date": "2019-10-12T00:00:00.000Z",
+            "answerer_name": "n00bgamer",
+            "helpfulness": 4,
+            "photos": []
+        }
+    }
+}
   ]
-};
+}
 
 export default mockData;
