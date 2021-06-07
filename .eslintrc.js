38578314bf52f79533663c4a684da83297ff6137
Alec <alecech1994@gmail.com>
2021-06-07 10:19:02 -0700

    Add React Hooks linter plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: 'airbnb',
+  extends: ['airbnb', 'plugin:react-hooks/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
