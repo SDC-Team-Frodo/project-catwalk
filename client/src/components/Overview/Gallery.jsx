1acab7456544bbca2eebbdb7c6734ab4bb56b8a8
Ivan Bailey <ivangbailey@gmail.com>
2021-06-08 16:42:57 -0700

    changing icons for expanding vs shrinking on gallery

--- client/src/components/Overview/Gallery.jsx
+++ client/src/components/Overview/Gallery.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 const Gallery = (props) => {
-
+  const ficon = props.fullscreenSlider ? 'compress' : 'expand';
   return (
     <div id="gallery" className={props.fullscreenSlider ? 'full' : ''}>
-      <i className="fas fa-expand float-right" onClick={() => props.setFullscreenSlider(!props.fullscreenSlider)}></i>
+      <i className={`fas fa-${ficon} float-right`} onClick={() => props.setFullscreenSlider(!props.fullscreenSlider)}></i>
     </div>
   );
 }
