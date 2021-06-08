9f48c8980de0cc79bd99f6b5d535bc87f5b4f96a
Francis Yao <neko.makie27@gmail.com>
2021-06-08 16:24:36 -0700

    Fixed error in testing

--- client/src/components/RelatedWidget/widget.test.js
+++ client/src/components/RelatedWidget/widget.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen, render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import RelatedContainer from './widget';
+import relatedSamples from './relatedSamples'; // delete later
 
 describe('Related component', () => {
   beforeAll(() => {
