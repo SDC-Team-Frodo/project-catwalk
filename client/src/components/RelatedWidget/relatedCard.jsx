66a6a093b6aa6febe7f33005c6110693a75974f5
Francis Yao <neko.makie27@gmail.com>
2021-06-08 14:44:41 -0700

    Added card layout

---
+++ client/src/components/RelatedWidget/relatedCard.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+const RelatedCard = (props) => {
+  const { product, thumbnail } = props;
+  const {
+    features, default_price, category, name,
+  } = product;
+
+  return (
+    <div className="relatedCard">
+      <div className="imageContainer">
+        <img src={thumbnail} alt="Failed" />
+        <div className="star">&#9734;</div>
+      </div>
+      <div className="textContainer">
+        {category}
+        <br />
+        <b>{name}</b>
+        <br />
+        {default_price}
+        <br />
+        <span>&#x2605;&#x2605;&#x2605;&#9734;&#9734;</span>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedCard;
--- client/src/components/RelatedWidget/widget.jsx
+++ client/src/components/RelatedWidget/widget.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import OutfitList from './outfitList';
+import RelatedList from './relatedList';
+import './relatedStyles.sass';
 
-const RelatedContainer = (props) => (
-<<<<<<< HEAD
-
-=======
->>>>>>> d98a874c8f393c23344d44d44979dee02a8a6496
-  <div id="Related">
-    <h1>this is the Related</h1>
-  </div>
-);
+const RelatedContainer = () => {
+  // const product = useContext(ProductContext);
+  // send API call for product to get features for modal window
+  // send API calls for each related product to get category, name, default_price, and features
+  // send to send API calls for each related product to get star rating
+  // send API calls for each outfit to get category, name, default_price
+  // send to send API calls for each outfit to get star rating
+  return (
+    <>
+      <RelatedList />
+      <OutfitList />
+    </>
+  );
+};
 
 export default RelatedContainer;
