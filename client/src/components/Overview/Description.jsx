d3d43126bae708a0f37b4e2fed848adcc81d1029
Ivan Bailey <ivangbailey@gmail.com>
2021-06-08 10:32:45 -0700

    Created a basic layout using flexbox. The subcomponents of the Overview component are also tested for rendering.

---
+++ client/src/components/Overview/Description.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Description = () => {
+  return (
+    <div id="description"></div>
+  );
+}
+
+export default Description;
---
+++ client/src/components/Overview/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Features = (props) => {
+  return (
+    <div id="features">
+
+    </div>
+  );
+};
+
+export default Features;
