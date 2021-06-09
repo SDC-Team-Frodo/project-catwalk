d4e4166335c782acd27fe683d532bde584fb3fb4
Francis Yao <neko.makie27@gmail.com>
2021-06-09 15:18:43 -0700

    Tested api calls for getting related product data, ratings, and thumbnail

--- client/src/components/RelatedWidget/relatedList.jsx
+++ client/src/components/RelatedWidget/relatedList.jsx
@@ -1,35 +1,57 @@
 import React, { useState, useEffect, useContext } from 'react';
 import RelatedCard from './relatedCard';
+import request from '../../requests';
+import ProductContext from '../../contexts/ProductContext';
 import relatedSamples from './relatedSamples'; // delete later
 import stylesSamples from './stylesSamples'; // delete later
 
 // eslint-disable-next-line func-names
 const RelatedList = function () {
+  const product = useContext(ProductContext);
   const numberOfCards = relatedSamples.length;
-  let index = 1;
-  let translateX = 0;
+  const [relatedlist, setRelatedlist] = useState([]);
+  const [index, setIndex] = useState(1);
+  const [translateX, setTranslateX] = useState(0);
+
+  useEffect(() => {
+    request.get(`products/${product.id}/related`, { endpoint: `products/${product.id}/related` })
+    // request.get(`reviews/meta`, { endpoint: `reviews/meta`, product_id: product.id })
+    // request.get(`products/${product.id}/styles`, { endpoint: `products/${product.id}/styles` })
+      .then((relatedProducts) => {
+        console.log('Successfully retrieved related:', relatedProducts.data);
+        // console.log('Successfully retrieved related:', relatedProducts.data.ratings);
+        // console.log('Successfully retrieved related:', relatedProducts.data.results[0].photos[0].thumbnail_url);
+        //fetch data for each related product
+        //fetch data for each related product thumbnail images
+        //fetch data for each related product star rating
+      }).catch((err) => {
+        console.log(err);
+      });
+  }, [product.id]);
 
   function buttonHandle(event) {
     const response = event.target.id;
-    let initial = 0;
 
     if (response === 'relatedPrevious') {
       if (index !== 1) {
-        index -= 1;
-        translateX += 270;
+        setIndex((previousIndex) => previousIndex - 1);
+        setTranslateX((previousTranslateX) => previousTranslateX + 270);
       }
     } else if (response === 'relatedNext') {
       if (index !== numberOfCards) {
-        index += 1;
-        translateX -= 270;
+        setIndex((previousIndex) => previousIndex + 1);
+        setTranslateX((previousTranslateX) => previousTranslateX - 270);
       }
     }
+  }
 
+  useEffect(() => {
+    let initial = 0;
     const cards = document.getElementsByClassName('relatedCard');
     for (initial; initial < cards.length; initial += 1) {
       cards[initial].style.transform = `translateX(${translateX}px`;
     }
-  }
+  }, [index, translateX]);
 
   return (
     <div id="related" >
