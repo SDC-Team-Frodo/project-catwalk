0b7de01a1b41336b790bc3f42d711a51ec899f72
Francis Yao <neko.makie27@gmail.com>
2021-06-08 22:22:31 -0700

    Added additional classes for carousel style scrolling

--- client/src/components/RelatedWidget/relatedList.jsx
+++ client/src/components/RelatedWidget/relatedList.jsx
@@ -3,12 +3,17 @@ import RelatedCard from './relatedCard';
 import relatedSamples from './relatedSamples'; // delete later
 import stylesSamples from './stylesSamples'; // delete later
 
-const RelatedList = function() {
+// eslint-disable-next-line func-names
+const RelatedList = function (props) {
   return (
-    <div id="related">
-      {relatedSamples.map((relatedProduct, index) => <RelatedCard product={relatedProduct} thumbnail={stylesSamples[index].photos[0].thumbnail_url} key={relatedProduct.id} />)}
+    <div id="related" >
+      <button type="button" className="carousel_button previous" id="previous">&#60;</button>
+      <div className="carousel" id="relatedList">
+        {relatedSamples.map((relatedProduct, index) => <RelatedCard product={relatedProduct} thumbnail={stylesSamples[index].photos[0].thumbnail_url} key={relatedProduct.id} />)}
+      </div>
+      <button type="button" className="carousel_button next" id="next">&#62;</button>
     </div>
-  )
-}
+  );
+};
 
 export default RelatedList;
