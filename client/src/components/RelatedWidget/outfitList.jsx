ffdd1d6c436bd910a0e31b365392c7ec480f12e0
Francis Yao <neko.makie27@gmail.com>
2021-06-10 11:03:26 -0700

    Added functionality for adding to local storage

--- client/src/components/RelatedWidget/outfitList.jsx
+++ client/src/components/RelatedWidget/outfitList.jsx
@@ -1,12 +1,77 @@
 import React, { useState, useEffect, useContext } from 'react';
-import BlankCard from './blankCard';
+import ProductContext from '../../contexts/ProductContext';
+import request from '../../requests';
+// import BlankCard from './blankCard';
 
-const OutfitList = function() {
-  let [ outfitItems, setOutfitItems ] = useState(undefined);
+const OutfitList = () => {
+  const product = useContext(ProductContext);
+
+  const [numberOfCards, setNumberOfCards] = useState(0);
+  const [translateX, setTranslateX] = useState(0);
+  const [outfitList, setOutfitList] = useState([]);
+  const [outfitRatings, setOutfitRatings] = useState([]);
+  const [outfitThumbnails, setOutfitThumbnails] = useState([]);
+  const [index, setIndex] = useState(1);
+
+  useEffect(() => {
+    outfitList.forEach((id) => {
+      request.get(`products/${id}`, { endpoint: `products/${id}` })
+        .then((newOutfitProduct) => {
+          setOutfitList((oldProducts) => [...oldProducts, newOutfitProduct.data]);
+        })
+        .catch((err) => console.log(err));
+
+      request.get(`reviews/meta`, { endpoint: `reviews/meta`, product_id: id })
+        .then((rating) => {
+          setOutfitRatings((oldRatings) => [...oldRatings, rating.data.ratings]);
+        })
+        .catch((err) => console.log(err));
+
+      request.get(`products/${id}/styles`, { endpoint: `products/${id}/styles` })
+        .then((thumbnail) => {
+          setOutfitThumbnails((oldThumbnails) => [...oldThumbnails, thumbnail.data.results[0].photos[0]]);
+        })
+        .catch((err) => console.log(err));
+    });
+  }, [outfitList]);
+
+  function addOutfit() {
+    let outfitListLocal = JSON.parse(localStorage.getItem('outfit'));
+
+    if (!outfitListLocal) {
+      outfitListLocal = [];
+    }
+
+    if (outfitListLocal.indexOf(product.id) === -1) {
+      outfitListLocal.push(product.id);
+      localStorage.setItem('outfit', JSON.stringify(outfitListLocal));
+      console.log(outfitListLocal)
+      setOutfitList(outfitListLocal);
+    }
+  }
+  //// DELETE THESE
+  function clearOutfitLocal () {
+    localStorage.removeItem('outfit')
+  }
+  function checkLocal () {
+    console.log('this is in local', JSON.parse(localStorage.getItem('outfit')))
+    console.log('this is in state', outfitList)
+  }
 
   return (
-    <BlankCard />
-  )
-}
+    <>
+    <div className="card plus" onClick={addOutfit} id="blank">
+      +
+      <br />
+      Add Current Product To
+      <br />
+      Outfits
+    </div>
+    <button type="button" onClick={clearOutfitLocal}>clear</button>
+
+    <button type="button" onClick={checkLocal}>check inside of local storage outfit</button>
+    </>
+  );
+};
 
 export default OutfitList;
