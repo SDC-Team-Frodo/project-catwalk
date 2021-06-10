414bda2f19ccc3e0ad3a343c3ff4248351ee5adf
Jacob Santala <JacobISantala@gmail.com>
2021-06-10 11:08:41 -0700

    Refactored the more answers button to dissapear when there are no more answers.

--- client/src/components/QAWidget/Question.jsx
+++ client/src/components/QAWidget/Question.jsx
@@ -7,13 +7,13 @@ const Question = (props) => {
   const [answers, setAnswers] = useState(
     Object.values(question.answers).slice(0, displayedAnswers),
   );
-  const [buttonLabel, setButtonLabel] = useState('More Answers');
+  const [buttonDisplay, setButtonDisplay] = useState(true);
 
   useEffect(() => {
     if (Object.values(question.answers)[displayedAnswers - 2] !== undefined) {
       setAnswers(Object.values(question.answers).slice(0, displayedAnswers));
     } else {
-      setButtonLabel('No More Answers');
+      setButtonDisplay(false);
     }
   }, [displayedAnswers]);
 
@@ -39,16 +39,18 @@ const Question = (props) => {
       <div>
         {answers.map((answer, i) => <Answer index={i} answer={answer} key={i} />)}
       </div>
-      <button
-        type="button"
-        id="loadA"
-        className="hoverGrey"
-        onClick={() => {
-          setDisplayedAnswers(displayedAnswers + 2);
-        }}
-      >
-        {buttonLabel}
-      </button>
+      {buttonDisplay && (
+        <button
+          type="button"
+          id="loadA"
+          className="hoverGrey"
+          onClick={() => {
+            setDisplayedAnswers(displayedAnswers + 2);
+          }}
+        >
+          More Answers
+        </button>
+      )}
     </div>
   );
 };
