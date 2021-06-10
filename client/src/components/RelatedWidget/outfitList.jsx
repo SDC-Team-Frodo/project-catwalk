9bcac0e6ead6e26433c4a08c7eb3c46bb03dcf36
Francis Yao <neko.makie27@gmail.com>
2021-06-10 11:17:03 -0700

    Added outfitProducts to store product data.
    outfitList stores the id in an array
    outfitProducts stores the product details in an array

--- client/src/components/RelatedWidget/outfitList.jsx
+++ client/src/components/RelatedWidget/outfitList.jsx
@@ -8,31 +8,36 @@ const OutfitList = () => {
 
   const [numberOfCards, setNumberOfCards] = useState(0);
   const [translateX, setTranslateX] = useState(0);
-  const [outfitList, setOutfitList] = useState([]);
+  const [outfitList, setOutfitList] = useState(JSON.parse(localStorage.getItem('outfit')));
+  const [outfitProducts, setOutfitProducts] = useState([]);
   const [outfitRatings, setOutfitRatings] = useState([]);
   const [outfitThumbnails, setOutfitThumbnails] = useState([]);
   const [index, setIndex] = useState(1);
 
   useEffect(() => {
-    outfitList.forEach((id) => {
-      request.get(`products/${id}`, { endpoint: `products/${id}` })
-        .then((newOutfitProduct) => {
-          setOutfitList((oldProducts) => [...oldProducts, newOutfitProduct.data]);
-        })
-        .catch((err) => console.log(err));
+    if (outfitList && outfitList.length > 0) {
+      outfitList.forEach((id) => {
+        request.get(`products/${id}`, { endpoint: `products/${id}` })
+          .then((newOutfitProduct) => {
+            setOutfitProducts((oldProducts) => [...oldProducts, newOutfitProduct.data]);
+          })
+          .catch((err) => console.log(err));
 
-      request.get(`reviews/meta`, { endpoint: `reviews/meta`, product_id: id })
-        .then((rating) => {
-          setOutfitRatings((oldRatings) => [...oldRatings, rating.data.ratings]);
-        })
-        .catch((err) => console.log(err));
+        // request.get(`reviews/meta`, { endpoint: `reviews/meta`, product_id: id })
+        //   .then((rating) => {
+        //     setOutfitRatings((oldRatings) => [...oldRatings, rating.data.ratings]);
+        //   })
+        //   .catch((err) => console.log(err));
 
-      request.get(`products/${id}/styles`, { endpoint: `products/${id}/styles` })
-        .then((thumbnail) => {
-          setOutfitThumbnails((oldThumbnails) => [...oldThumbnails, thumbnail.data.results[0].photos[0]]);
-        })
-        .catch((err) => console.log(err));
-    });
+        // request.get(`products/${id}/styles`, { endpoint: `products/${id}/styles` })
+        //   .then((thumbnail) => {
+        //     setOutfitThumbnails((oldThumbnails) => [...oldThumbnails, thumbnail.data.results[0].photos[0]]);
+        //   })
+        //   .catch((err) => console.log(err));
+      });
+    } else {
+      setOutfitList([]);
+    }
   }, [outfitList]);
 
   function addOutfit() {
