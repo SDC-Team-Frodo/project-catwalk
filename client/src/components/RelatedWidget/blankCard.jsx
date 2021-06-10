d79c411046bda5c1c6090832eea0eb1f455e4562
Francis Yao <neko.makie27@gmail.com>
2021-06-10 11:02:44 -0700

    Functionality moved to outFit list

--- client/src/components/RelatedWidget/blankCard.jsx
+++ client/src/components/RelatedWidget/blankCard.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProductContext from '../../contexts/ProductContext';
 
-const BlankCard = () => (
-  <div className="card plus">
-    +
-  </div>
-);
+const BlankCard = () => {
+  const product = useContext(ProductContext)
+
+  function addOutfit() {
+    // document.getElementById('blank').style.display = 'none';
+    console.log(product.id)
+
+    localStorage.setItem('outfit', product.id);
+    console.log('this is in local storage', localStorage.getItem('outfit'))
+
+  }
+
+  return (
+    <div className="card plus" onClick={addOutfit} id="blank">
+      +
+      <br />
+      Add Current Product To
+      <br />
+      Outfits
+    </div>
+  );
+};
 
 export default BlankCard;
