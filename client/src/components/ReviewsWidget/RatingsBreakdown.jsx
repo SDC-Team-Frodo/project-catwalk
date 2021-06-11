2c68d370bd44e81dd70d707999f3cec26e8aa23b
Alec <alecech1994@gmail.com>
2021-06-11 15:59:18 -0700

    Attempts to modify tests to work with context

--- client/src/components/ReviewsWidget/RatingsBreakdown.jsx
+++ client/src/components/ReviewsWidget/RatingsBreakdown.jsx
@@ -1,18 +1,32 @@
 import React, { useEffect, useContext } from 'react';
 import ReactStars from 'react-rating-stars-component';
 import RatingContext from '../../contexts/RatingContext';
+import FilterContext from './FilterContext';
 import ProgressBar from './ProgressBar';
 import getAverageRating from '../../helpers/averageRating';
 import getTotalRatings from '../../helpers/totalRatings';
 
 const RatingsBreakdown = ({ ratings }) => {
   const [averageRating, setAverageRating] = useContext(RatingContext);
+  const [filteredContext, setFilteredContext] = useContext(FilterContext);
   useEffect(() => {
     setAverageRating(getAverageRating(ratings));
   }, [ratings]);
   return (
     <section id="ratings-breakdown">
       <h4>RATINGS</h4>
+      {filteredContext.length !== 0
+      && (
+        <div className="filter-summary">
+          Filtering results by:
+          <div className="current-filters">
+            {filteredContext.map((rating) => `${rating} stars`).join(', ')}
+          </div>
+          <button type="button" onClick={() => setFilteredContext([])}>
+            Remove All Filters
+          </button>
+        </div>
+      )}
       <div className="avg-rating-stars">
         <div className="avg-rating">{averageRating}</div>
         <div>
