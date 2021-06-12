b8e0f1e05d459d232966667e682b3bdf838a9f2f
Jacob Santala <JacobISantala@gmail.com>
2021-06-12 10:19:31 -0700

    Changed some things up with the Forms when you send the button turns into a sent button.

--- client/src/components/QAWidget/AnswerForm.jsx
+++ client/src/components/QAWidget/AnswerForm.jsx
@@ -50,87 +50,84 @@ const AnswerForm = (props) => {
     nickName.length > 2 ? setValidN(true) : setValidN(false);
     isEmail(email) ? setValidE(true) : setValidE(false);
   }, [answer, nickName, email]);
-  if (!displaySent) {
-    return (
-      <div id="answerForm">
-        <div>
-          <label className="formInput" htmlFor="Answer">
-            Your Answer
-            <span className={validA ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
-          </label>
-          <textarea
-            rows="3"
-            cols="100"
-            wrap="hard"
-            id="Question"
-            placeholder="Write your answer here"
-            value={answer}
-            maxLength="1000"
-            onChange={(e) => {
-              setAnswer(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label className="formInput" htmlFor="nickName">
-            What Is Your Nickname
-            <span className={validN ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
-          </label>
-          <input
-            type="text"
-            id="nickName"
-            placeholder="Example: jack543!!"
-            value={nickName}
-            onChange={(e) => {
-              setNickName(e.target.value);
-            }}
-          />
-          <h4 className="disclaimer">*For privacy reasons, do not use your full name or email address*</h4>
-        </div>
-        <div>
-          <label className="formInput" htmlFor="email">
-            Your Email
-            <span className={validE ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
-          </label>
-          <input
-            type="text"
-            id="email"
-            placeholder="Example: billbillbill@email.com"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <h4 className="disclaimer">*For authentication reasons, you will not be emailed*</h4>
-        </div>
-        {/* <div>
-          <label className="formInput" htmlFor="image">
-            Upload Photo
-          </label>
-          <input
-            type="text"
-            id="image"
-            alt="none"
-            placeholder="Enter a URL to your photo"
-            onChange={(e) => {
-              setPhotos([...photos, e.target.value]);
-            }}
-          />
-        </div> */}
-        <button
-          className="hoverGrey"
-          type="button"
-          id="submitABtn"
-          onClick={() => {
-            setSubClicked(true);
+  return (
+    <div id="answerForm">
+      <div>
+        <label className="formInput" htmlFor="Answer">
+          Your Answer
+          <span className={validA ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
+        </label>
+        <textarea
+          rows="3"
+          cols="100"
+          wrap="hard"
+          id="Question"
+          placeholder="Write your answer here"
+          value={answer}
+          maxLength="1000"
+          onChange={(e) => {
+            setAnswer(e.target.value);
           }}
-        >
-          Submit!
-        </button>
+        />
       </div>
-    );
-  }
-  return <h1 className="sent">Sent</h1>;
+      <div>
+        <label className="formInput" htmlFor="nickName">
+          What Is Your Nickname
+          <span className={validN ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
+        </label>
+        <input
+          type="text"
+          id="nickName"
+          placeholder="Example: jack543!!"
+          value={nickName}
+          onChange={(e) => {
+            setNickName(e.target.value);
+          }}
+        />
+        <h4 className="disclaimer">*For privacy reasons, do not use your full name or email address*</h4>
+      </div>
+      <div>
+        <label className="formInput" htmlFor="email">
+          Your Email
+          <span className={validE ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
+        </label>
+        <input
+          type="text"
+          id="email"
+          placeholder="Example: billbillbill@email.com"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <h4 className="disclaimer">*For authentication reasons, you will not be emailed*</h4>
+      </div>
+      {/* <div>
+        <label className="formInput" htmlFor="image">
+          Upload Photo
+        </label>
+        <input
+          type="text"
+          id="image"
+          alt="none"
+          placeholder="Enter a URL to your photo"
+          onChange={(e) => {
+            setPhotos([...photos, e.target.value]);
+          }}
+        />
+      </div> */}
+      <button
+        className="hoverGrey"
+        type="button"
+        id="submitABtn"
+        onClick={() => {
+          setSubClicked(true);
+        }}
+      >
+        {displaySent ? 'Sent' : 'Submit!'}
+      </button>
+    </div>
+  );
 };
 
 export default AnswerForm;
--- client/src/components/QAWidget/QuestionForm.jsx
+++ client/src/components/QAWidget/QuestionForm.jsx
@@ -53,74 +53,74 @@ const QuestionForm = () => {
     isEmail(email) ? setValidE(true) : setValidE(false);
   }, [question, nickName, email]);
 
-  if (!displaySent) {
-    return (
-      <div id="questionForm">
-        <div>
-          <label className="formInput" htmlFor="Question">
-            Your Question
-            <span className={validQ ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
-          </label>
-          <textarea
-            rows="3"
-            cols="100"
-            wrap="hard"
-            id="Question"
-            value={question}
-            placeholder="Write your question here"
-            maxLength="1000"
-            onChange={(e) => {
-              setQuestion(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label className="formInput" htmlFor="nickName">
-            What Is Your Nickname
-            <span className={validN ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
-          </label>
-          <input
-            type="text"
-            id="nickName"
-            placeholder="Example: jackson11!"
-            value={nickName}
-            onChange={(e) => {
-              setNickName(e.target.value);
-            }}
-          />
-          <h4 className="disclaimer">*For privacy reasons, do not use your full name or email address*</h4>
-        </div>
-        <div>
-          <label className="formInput" htmlFor="email">
-            Your Email
-            <span className={validE ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Example: billbillbill@email.com"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <h4 className="disclaimer">*For authentication reasons, you will not be emailed*</h4>
-        </div>
-        <button
-          className="hoverGrey"
-          type="button"
-          id="submitQBtn"
-          onClick={() => {
-            setSubClicked(true);
-            setDisplaySent(true);
+  // if (!displaySent) {
+  return (
+    <div id="questionForm">
+      <div>
+        <label className="formInput" htmlFor="Question">
+          Your Question
+          <span className={validQ ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
+        </label>
+        <textarea
+          rows="3"
+          cols="100"
+          wrap="hard"
+          id="Question"
+          value={question}
+          placeholder="Write your question here"
+          maxLength="1000"
+          onChange={(e) => {
+            setQuestion(e.target.value);
           }}
-        >
-          Submit!
-        </button>
+        />
       </div>
-    );
-  }
-  return <h1 className="sent">Sent</h1>;
+      <div>
+        <label className="formInput" htmlFor="nickName">
+          What Is Your Nickname
+          <span className={validN ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
+        </label>
+        <input
+          type="text"
+          id="nickName"
+          placeholder="Example: jackson11!"
+          value={nickName}
+          onChange={(e) => {
+            setNickName(e.target.value);
+          }}
+        />
+        <h4 className="disclaimer">*For privacy reasons, do not use your full name or email address*</h4>
+      </div>
+      <div>
+        <label className="formInput" htmlFor="email">
+          Your Email
+          <span className={validE ? 'valid' : 'inValid'}>{' * Mandatory'}</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Example: billbillbill@email.com"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <h4 className="disclaimer">*For authentication reasons, you will not be emailed*</h4>
+      </div>
+      <button
+        className="hoverGrey"
+        type="button"
+        id="submitQBtn"
+        onClick={() => {
+          setSubClicked(true);
+          setDisplaySent(true);
+        }}
+      >
+        {displaySent ? 'Sent!' : 'Submit!'}
+      </button>
+    </div>
+  );
+  // }
+  // return <h1 className="sent">Sent</h1>;
 };
 
 export default QuestionForm;
