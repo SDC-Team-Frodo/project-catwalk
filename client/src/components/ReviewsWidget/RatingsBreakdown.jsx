3a6620fde27d2caa994e3d920c0291dfbf457977
Alec <alecech1994@gmail.com>
2021-06-11 22:47:49 -0700

    Wrote own star components that display star value to the nearest quarter

--- client/src/components/ReviewsWidget/RatingsBreakdown.jsx
+++ client/src/components/ReviewsWidget/RatingsBreakdown.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react';
-import ReactStars from 'react-rating-stars-component';
+import React, { useEffect, useContext } from 'react';
+// import ReactStars from 'react-rating-stars-component';
 import RatingContext from '../../contexts/RatingContext';
 import FilterContext from './FilterContext';
 import ProgressBar from './ProgressBar';
@@ -9,18 +9,9 @@ import getTotalRatings from '../../helpers/totalRatings';
 const RatingsBreakdown = ({ ratings }) => {
   const [averageRating, setAverageRating] = useContext(RatingContext);
   const [filteredContext, setFilteredContext] = useContext(FilterContext);
-  const [rendered, setRendered] = useState(false);
-  useEffect(() => {
-    if (rendered) {
-      setAverageRating(null);
-      setRendered(false);
-    } else {
-      setRendered(true);
-    }
-  }, [ratings]);
   useEffect(() => {
     setAverageRating(getAverageRating(ratings));
-  }, [rendered]);
+  }, [ratings]);
   return (
     <section id="ratings-breakdown">
       <h4>RATINGS</h4>
@@ -40,18 +31,14 @@ const RatingsBreakdown = ({ ratings }) => {
       && (
       <div className="avg-rating-stars">
         <div className="avg-rating">{averageRating}</div>
-        <div>
-          <ReactStars {
-            ...{
-              size: 16,
-              value: Math.round(averageRating * 2) / 2,
-              a11y: true,
-              isHalf: true,
-              edit: false,
-              activeColor: 'red',
-            }
-          }
-          />
+        <div className="empty-stars">
+          <div className="filled-stars" style={{ width: `${(Math.round(averageRating * 4) / 4) * 20}%` }}>
+            <i className="fas fa-star" aria-hidden="true" />
+            <i className="fas fa-star" aria-hidden="true" />
+            <i className="fas fa-star" aria-hidden="true" />
+            <i className="fas fa-star" aria-hidden="true" />
+            <i className="fas fa-star" aria-hidden="true" />
+          </div>
         </div>
       </div>
       )}
