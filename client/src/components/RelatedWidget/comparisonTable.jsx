3727dd2cf806b14ffc888aa2ef457109d5234e6a
Francis Yao <neko.makie27@gmail.com>
2021-06-12 11:25:56 -0700

    Renamed productName to cardName

--- client/src/components/RelatedWidget/comparisonTable.jsx
+++ client/src/components/RelatedWidget/comparisonTable.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import TableEntry from './tableEntry';
 
 const ComparisonTable = (props) => {
-  const { overviewName, productName, combined } = props;
+  const { overviewName, cardName, combined } = props;
   return (
     <table className="compareContent">
       <thead>
         <tr>
           <th>{overviewName}</th>
           <th>Characteristics</th>
-          <th>{productName}</th>
+          <th>{cardName}</th>
         </tr>
       </thead>
       <tbody>
