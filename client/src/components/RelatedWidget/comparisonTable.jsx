9959e9613b411d417fe46a2e19d9d30f4ed1aea6
Francis Yao <neko.makie27@gmail.com>
2021-06-11 19:17:08 -0700

    Added component to generate table

---
+++ client/src/components/RelatedWidget/comparisonTable.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import TableEntry from './tableEntry';
+
+const ComparisonTable = (props) => {
+  const { overviewName, productName, combined } = props;
+  return (
+    <table className="comparisonWindow">
+      <thead>
+        <tr>
+          <th>{overviewName}</th>
+          <th>Characteristics</th>
+          <th>{productName}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          Object.keys(combined).map((keys, i) => <TableEntry key={`${keys}${i}`} keys={keys} array={combined[keys]} />)
+        }
+      </tbody>
+    </table>
+
+  );
+};
+
+export default ComparisonTable;
