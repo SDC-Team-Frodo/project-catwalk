824d3816a8058231eff51929aed62536f4aea965
Francis Yao <neko.makie27@gmail.com>
2021-06-11 19:19:57 -0700

    Created helper file to store function Store button function here when refactoring

--- client/src/components/RelatedWidget/relatedHelpers.js
+++ client/src/components/RelatedWidget/relatedHelpers.js
@@ -1,13 +1,22 @@
 const helpers = {
-  compareFeatures(...features) {
-    const [feature1, feature2] = features;
+  compareFeatures(arrFeat1, arrFeat2) {
     const combinedFeatures = {};
+    const feature1 = {};
+    const feature2 = {};
+
+    arrFeat1.forEach((index) => {
+      feature1[index.feature] = index.value;
+    });
+    arrFeat2.forEach((index) => {
+      feature2[index.feature] = index.value;
+    });
+
     Object.keys(feature1).forEach((key) => {
       if (feature1[key] && feature2[key]) {
         combinedFeatures[key] = [feature1[key], feature2[key]];
         delete feature2[key];
       } else {
-        combinedFeatures[key] = [feature1[key], '']
+        combinedFeatures[key] = [feature1[key], ''];
       }
     });
     Object.keys(feature2).forEach((key) => {
