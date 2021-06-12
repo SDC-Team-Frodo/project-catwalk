4fa77d257cc3dd289696c9b20e921bdec80198d3
Francis Yao <neko.makie27@gmail.com>
2021-06-12 10:03:51 -0700

    Removed create modal window functionality

--- client/src/components/RelatedWidget/card.jsx
+++ client/src/components/RelatedWidget/card.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactStars from 'react-rating-stars-component';
 import getAverageRating from '../../helpers/averageRating';
-import helpers from './relatedHelpers';
-import Modal from '../Modal';
-import ComparisonTable from './comparisonTable';
 
 const Card = (props) => {
   const {
-    product, thumbnail, ratings, cardClass, isStars, func, overview,
+    product, thumbnail, ratings, cardClass, isStars, func,
   } = props;
 
   const {
@@ -25,26 +22,14 @@ const Card = (props) => {
     const background = {
       backgroundImage: `url("${thumbnail.thumbnail_url}")`,
     };
-    const combined = helpers.compareFeatures(overview.features, product.features);
     return (
       <div className={cardClasses}>
         <div className="imageContainer" style={background} onClick={redirect}>
           <img src={thumbnail.thumbnail_url} alt="Failed" />
         </div>
-        <div className="cardIcon" id={iconId}>
-          {!isStars ? <span onClick={func} className="cardIcon" id={iconId}>&#9447;</span>
-            : (
-              <Modal
-                modalId={iconId}
-                header={<p>Comparing</p>}
-                body={<ComparisonTable combined={combined} overviewName={overview.name} productName={product.name} />}
-                footer={<div />}
-                btnName=""
-                btnId=""
-                isImage={true}
-                image={<span className="cardIcon" id={iconId}>&#x2605;</span>}
-              />
-            )}
+        <div className="cardIcon" id={iconId} onClick={func}>
+          {!isStars ? <span className="cardIcon" id={iconId}>&#9447;</span>
+            : <span className="cardIcon" id={iconId}>&#x2605;</span>}
         </div>
         <div className="textContainer" onClick={redirect}>
           {category}
