f2ccfd88e4839258fbea520ba189503b67873c24
Jacob Santala <JacobISantala@gmail.com>
2021-06-14 12:01:19 -0700

    Added a next and previous button on the browse page.

--- client/src/components/Browse/BrowsePage.jsx
+++ client/src/components/Browse/BrowsePage.jsx
@@ -5,19 +5,12 @@ import allProducts from './AllProducts';
 
 const Browse = (props) => {
   const { setProduct } = props;
-  const [products, setProducts] = useState(allProducts);
+  const [displayedProducts, setDisplayedProducts] = useState([0, 10]);
+  const [products, setProducts] = useState(allProducts.slice(...displayedProducts));
 
-  // useEffect(() => {
-  //   if (products.length === 0) {
-  //     request.get('products', {
-  //       count: 200,
-  //     }).then((res) => {
-  //       setProducts(res.data);
-  //     }).catch((err) => {
-  //       console.error(err);
-  //     });
-  //   }
-  // }, [products]);
+  useEffect(() => {
+    setProducts(allProducts.slice(...displayedProducts));
+  }, [displayedProducts]);
 
   return (
     <div id="BrowseList">
@@ -25,6 +18,29 @@ const Browse = (props) => {
       {products.map((product, i) => {
         return <Product id={product.id} setProduct={setProduct} key={i} name={product.name} />;
       })}
+      {displayedProducts[0] > 0 && (
+        <button
+          type="button"
+          id="previouseBrowse"
+          className="hoverGrey"
+          onClick={() => {
+            setDisplayedProducts([displayedProducts[0] - 10, displayedProducts[1] - 10]);
+          }}
+        >
+          {"<"}
+        </button>
+      )}
+      <button
+        type="button"
+        id="nextBrowse"
+        className="hoverGrey"
+        onClick={() => {
+          setDisplayedProducts([displayedProducts[0] + 10, displayedProducts[1] + 10]);
+        }}
+      >
+        {">"}
+      </button>
+      <h3 id="curProductCount">{`${displayedProducts[0]} to ${displayedProducts[1]}`}</h3>
     </div>
   );
 };
