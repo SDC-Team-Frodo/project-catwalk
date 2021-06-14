7cc514d8800d446ff5ef7336782fce952591d58f
Alec <alecech1994@gmail.com>
2021-06-14 16:11:44 -0700

    Updated labels on characteristic bars

--- client/src/components/ReviewsWidget/CharLabels.jsx
+++ client/src/components/ReviewsWidget/CharLabels.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const CharLabels = ({ char }) => {
-  const labels = ['Size', 'Width', 'Length'].includes(char) ? ['Too small', 'Perfect', 'Too big'] : ['Poor', 'Great'];
+  const labels2 = {
+    Size: ['Too small', 'Perfect', 'Too big'],
+    Width: ['Too narrow', 'Perfect', 'Too wide'],
+    Length: ['Too short', 'Too long'],
+    Fit: ['Too tight', 'Perfect', 'Too long'],
+    Comfort: ['Poor', 'Great'],
+    Quality: ['Poor', 'Great'],
+  };
   return (
     <div className="char-labels">
-      {labels.map((label) => (<div key={label}>{label}</div>))}
+      {labels2[char].map((label) => (<div key={label}>{label}</div>))}
     </div>
   );
 };
