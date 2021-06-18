655fb20032c49b17bae99ffec2ad0ce5601f159e
Francis Yao <neko.makie27@gmail.com>
2021-06-18 09:59:38 -0700

    Removed sample styles import in tests

--- client/src/components/RelatedWidget/widget.test.js
+++ client/src/components/RelatedWidget/widget.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { screen, render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import RelatedContainer from './widget';
-import relatedSamples from './relatedSamples'; // delete later
-import stylesSamples from './stylesSamples'; // delete later
 
 describe('Related component', () => {
   beforeAll(() => {
